fb590b5385ff85abe34b0058e0831ae8e42ba6a5
UmmeNafisa <pappanafisa@gmail.comm>
2021-10-31 14:00:31 +0600

    use

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://umme:MlNla1KyR9j74QYR@cluster0.jo0ws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jo0ws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jo0ws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 console.log(uri);
 
 
