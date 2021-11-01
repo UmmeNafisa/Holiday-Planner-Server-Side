f705c91458a06c36a6c3eba3d2742d9b1f88e63a
UmmeNafisa <pappanafisa@gmail.comm>
2021-11-01 20:31:29 +0600

    update

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ async function run() {
         const database = client.db("holiday-planner");
         const packageCollection = database.collection("packages");
         const userBookedCollection = database.collection("userBooked")
-        const confirmOrderCollection =  database.collection("OrderConfirm")
+        const confirmOrderCollection = database.collection("OrderConfirm")
+        const finalOrderConfirmCollection = database.collection("Final-Order_confirm")
 
         //add packages
         app.post("/addPackages", async (req, res) => {
@@ -41,7 +42,7 @@ async function run() {
         // get clickable package
         app.get('/addPackages/:id', async (req, res) => {
             let id = req.params.id;
-            const result = await packageCollection.find({ _id: ObjectId(id)}).toArray();
+            const result = await packageCollection.find({ _id: ObjectId(id) }).toArray();
             res.send(result)
         })
 
@@ -53,17 +54,17 @@ async function run() {
 
         //get the book item from client page 
         app.get('/bookItems', async (req, res) => {
-        const result = await userBookedCollection.find({}).toArray();
-             res.json(result)
+            const result = await userBookedCollection.find({}).toArray();
+            res.json(result)
         })
 
-    //delete items
+        //delete items
         app.delete("/bookItems/:id", async (req, res) => {
-        const id = req.params.id;
-        
-        const result = await userBookedCollection.deleteOne({_id: req.params.id });
-        res.send(result);
-  });
+            const id = req.params.id;
+
+            const result = await userBookedCollection.deleteOne({ _id: req.params.id });
+            res.send(result);
+        });
 
         //add Client address and details
         app.post("/confirmBooking", async (req, res) => {
@@ -72,12 +73,47 @@ async function run() {
             // console.log(result);
             res.send(result)
         })
-        
+
         //get Client address and details
         app.get('/confirmBooking', async (req, res) => {
-        const result = await confirmOrderCollection.find({}).toArray();
-             res.json(result)
+            const result = await confirmOrderCollection.find({}).toArray();
+            res.json(result)
         })
+
+        //Final order confirmation post
+        app.post("/finalConfirmation", async (req, res) => {
+            console.log(req.body)
+            const result = await finalOrderConfirmCollection.insertOne(req.body);
+            res.send(result)
+        })
+
+        //get the final order data 
+        app.get('/finalConfirmation', async (req, res) => {
+            const result = await finalOrderConfirmCollection.find({}).toArray();
+            res.json(result)
+        })
+
+        //update status 
+        app.put('/finalConfirmation/:id', async (req, res) => {
+            const { id } = req.params;
+            const result = await finalOrderConfirmCollection.updateOne(
+                { upsert: true },
+                { $set: { status: "Approve" } }
+
+            );
+            console.log('updatingstatus', result)
+        });
+
+        //delete admin side data 
+        app.delete('/finalConfirmation/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const result = await finalOrderConfirmCollection.deleteOne({ _id: req.params.id });
+            // console.log(result);
+            res.send(result);
+        });
+
+
         // console.log("all are working perfectly");
 
 
