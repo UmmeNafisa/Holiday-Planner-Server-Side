af58cef011e792e18a965557a069da694f243c0f
UmmeNafisa <pappanafisa@gmail.comm>
2021-11-12 02:46:53 +0600

    delete button

--- index.js
+++ index.js
@@ -109,12 +109,11 @@ async function run() {
 
         //delete admin side data 
         app.delete('/finalConfirmation/:id', async (req, res) => {
-            // const id = req.params.id;
-            // console.log(id);
-            // const query = { _id: ObjectId(id) };
-
-            const result = await finalOrderConfirmCollection.deleteOne({ _id: req.params.id });
-            // console.log(result);
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await finalOrderConfirmCollection.deleteOne(query);
+            console.log(result);
             res.json(result);
         });
 
